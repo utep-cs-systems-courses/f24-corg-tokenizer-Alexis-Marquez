82aaf7e0b953163c67e6cc7fb309aa912b9d4805
Alexis Marquez <alexismarq337@gmail.com>
2024-10-11 22:50:25 -0600

    Finished history portion of project

---
+++ src/history.c
@@ -0,0 +1,55 @@
+#include "history.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+List* init_history(){
+  List *list = (List *)malloc(sizeof(List));
+  list->root = NULL;
+  return list;
+}
+void add_history(List *list, char *str){
+  Item *currItem = list->root;
+  Item *newItem = malloc(sizeof(Item));
+  newItem->str = strdup(str);
+  newItem->next = NULL;
+  if(list->root == NULL){
+    newItem->id = 1;
+    list->root = newItem;
+    return;
+  }
+  while(currItem->next != NULL){
+    currItem = currItem->next;
+  }
+  newItem->id = currItem->id + 1;
+  currItem->next = newItem;
+}
+char *get_history(List *list, int id){
+  Item *currItem = list->root;
+  int i = 1;
+  while(currItem != NULL && i<id){
+    currItem = currItem->next;
+    i++;
+  }
+  if(currItem==NULL){
+    return NULL;
+  }
+  return currItem->str;
+}
+void print_history(List *list){
+  Item *currItem = list->root;
+  while(currItem != NULL){
+    printf("%i %s\n",currItem->id , currItem->str);
+    currItem = currItem->next;
+  }
+}
+void free_history(List *list){
+  Item *currItem = list->root;
+  Item *nextItem;
+  while(currItem != NULL) {
+      nextItem = currItem->next;
+      free(currItem);
+      currItem = nextItem;
+    }
+    free(list);
+}
--- src/uiMain.c
+++ src/uiMain.c
@@ -3,20 +3,43 @@
 #include <stdlib.h>
 #include "tokenizer.c"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "history.c"
+#include "history.h"
 
 int main(){
+  List *list = init_history();
+  char *str;
   while(1){
-  char str[100];
-  printf("Enter string: ");
-  fgets(str, sizeof(str), stdin);
+  str = malloc(300);
+  printf("Enter string to tokenize, history to see previous strings, or q to exit: ");
+  fgets(str, 300, stdin);
   str[strcspn(str,"\n")]=0;
   if(strcmp(str,"q")==0){
     printf("Exit\n");
     return 1;
-  } 
+  }
+  if(*str == '!'){
+    str++;
+    str = get_history(list, atoi(str));
+    if(str==NULL){
+      printf("Invalid number, enter a valid index\n");
+      continue;
+    }
+  }
+  if(strcmp(str,"history")==0){
+    printf("History: \n");
+    print_history(list);
+  }
+  else{
   printf("You entered: %s\n", str);
+  add_history(list, str);
   char **tokens = tokenize(str);
   print_tokens(tokens);
   free_tokens(tokens);
   }
+  }
+  free_history(list);
+  free(str);
 }
